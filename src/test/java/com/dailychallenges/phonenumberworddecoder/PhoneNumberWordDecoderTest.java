cfc0fc057ba5270c63a7e3d812100da90ddc0f3b
just-do-git-projects <veronika.bellanova@online.de>
2021-02-10 17:20:16 +0100

    Added Phone Number Word Decoder

---
+++ src/test/java/com/dailychallenges/phonenumberworddecoder/PhoneNumberWordDecoderTest.java
@@ -0,0 +1,30 @@
+package com.dailychallenges.phonenumberworddecoder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class PhoneNumberWordDecoderTest {
+	
+	@ParameterizedTest
+	@CsvSource({"123-647-3937,123-647-EYES", "(325)444-8378,(325)444-TEST", "653-879-8447,653-TRY-THIS",
+		"435-224-7613,435-224-7613", "(333)668-3245,(33D)ONT-FAIL", "(025)445-6741,(025)445-6741"})
+	public void decodePhoneNumber(String expected, String actualParam) {
+		assertEquals(expected, PhoneNumberWordDecoder.textToNum(actualParam));
+	}
+	
+	@ParameterizedTest
+	@ValueSource(strings = {"", "  13x"})
+	public void errorDecodingPhoneNumber(String parameter) {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> PhoneNumberWordDecoder.textToNum(parameter));
+	}
+	
+	@Test
+	public void errorDecodingPhoneNumber() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> PhoneNumberWordDecoder.textToNum(null));
+	}
+}
