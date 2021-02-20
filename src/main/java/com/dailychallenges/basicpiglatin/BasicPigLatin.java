4ba81dbff2afd4e6fde64993c9b5632572653d27
just-do-git-projects <veronika.bellanova@online.de>
2021-02-20 11:50:28 +0100

    Basic Pig Latin: used regex

--- src/main/java/com/dailychallenges/basicpiglatin/BasicPigLatin.java
+++ src/main/java/com/dailychallenges/basicpiglatin/BasicPigLatin.java
@@ -1,26 +1,34 @@
 package com.dailychallenges.basicpiglatin;
 
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /** https://edabit.com/challenge/2aajqTB69y7ZSSo9v */
 public class BasicPigLatin {
-	
+
 	public static String pigLatin(String str) {
-		String interpunction = str.substring(str.length()-1);
-		String sentence = Stream.of(str.replace(interpunction,"").split(" "))
-				.map(w -> modify(w)).reduce("", (w1,w2) -> w1+w2+" ").trim();
-		
-		return sentence + interpunction;
+		return Stream.of(str.split(" ")).map(s -> isFirstVowel(s) ? addWay(s) : addAy(s))
+				.map(s -> containsUpperCase(s) ? capitalize(s) : s).collect(Collectors.joining(" "));
 	}
-	
-	private static String modify(String str) {
-		boolean uppercase = Character.isUpperCase(str.charAt(0));
-		String word = str.toLowerCase();
-		
-		word = word.substring(0,1).matches("[aeiou]") ? word + "way" :
-				word.substring(1) + word.charAt(0) + "ay";
-		
-		return !uppercase ? word :
-			word.substring(0,1).toUpperCase() + word.substring(1);
+
+	private static boolean isFirstVowel(String s) {
+		return s.matches("[aeiouAEIOU].*");
 	}
+
+	private static String addWay(String s) {
+		return s.replaceAll("(\\w+)(\\p{Punct}?)", "$1" + "way" + "$2");
+	}
+
+	private static String addAy(String s) {
+		return s.replaceAll("(\\w)(\\w+)(\\p{Punct}?)", "$2$1" + "ay" + "$3");
+	}
+
+	private static boolean containsUpperCase(String s) {
+		return s.matches("\\w*\\p{Upper}\\w*\\p{Punct}?");
+	}
+
+	private static String capitalize(String s) {
+		return s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
+	}
+
 }
